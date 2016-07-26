c2f3788c4b9e02525d05def08a22fdacf2f2c883
Dylan Culfogienis <dylanculfogienis@gmail.com>
2016-07-26 13:49:10 -0400

    Finished conversion to Python + solidpython
    - After an informal test, code generated by solidpython and native
    OpenSCAD have the same compile time. Solidpython code is nearly
    unreadable, however.
    - This will allow for an inline bill of materials
    - More complex code should be possible.

--- Waterpump.py
+++ Waterpump.py
@@ -49,6 +49,14 @@ spoolouternom = 1.25
 spoolouterod = 1.660
 spoolouterww = 0.140
 
+"""---Utils---"""
+### There is no "empty" OpenSCAD object, but to write the equivalent of
+### OpenSCAD for loops in Python, you need one. The SolidPython examples
+### assign an initial variable to a union() and add() to it. This alias
+### is simply for clarity's sake.
+def empty():
+    return union()
+
 """---General Plumbing---"""
 
 def tube(h, od, id, center=False):
@@ -213,12 +221,188 @@ def holes():
         )
     )
 
+"""---Spool Valves---"""
+
+#Note: Only works for even-numbered counts.
+#Unused, included as it was in the original .scad file
+def cylinder_star(holewidth, diameter, count):
+    out = empty()
+    for i in range(0, count/2 + 1):
+        out.add(
+            rotate([0, 90, i*360/count])(
+                cylinder(h = diameter*1.1, r = holewidth/2, center=True)
+            )
+        )
+
+    return out 
+
+def spool(length, od, ww, chambers, center=True):
+    l = length - ww
+    r = 0.1
+    c = 0
+    if center:
+        c = -length/2
+
+    def rods():
+        out = empty()
+        for i in range(0, 3):
+            out.add(
+                rotate([0, 0, i*120])(
+                    right(od/4)(
+                        cylinder(h = length, r = r)
+                    )
+                )
+            )
+        return out 
+
+    def pistons():
+        out = empty()
+        for i in range(0, chambers + 1):
+            out.add(
+                up(i*l/chambers + ww/2)(
+                    piston(od, ww)
+                )
+            )
+        return out 
+
+    if exploded == 0:
+        out = rods() + pistons()
+    else:
+        out = up(exploded*(length + 1))(
+            up(exploded*(length + 1))(
+                rods() + pistons()
+            )
+            +
+            rods()
+        )
+    return up(c)(
+        out
+    )
+
+def five_valve(length, od, oww, id, iww, top=True, center=True, endpadding=0):
+
+    def endports():
+        return (
+            up(length/2)(
+                fitting(h = oww*4, d=fittingdiameter, center=True)
+            )
+            +
+            down(length/2)(
+                fitting(h = oww*4, d=fittingdiameter, center=True)
+            )
+        )
+
+    def midports():
+        def ports():
+            out = empty()
+            for i in range(-2, 3):
+                j = 1
+                if i % 2 == 0:
+                    j = -1
+
+                out.add(
+                    translate([0, i*length/7, j*(od/2 - oww)])(
+                        fitting(h = oww*3, d=fittingdiameter, center=True)
+                    )
+                )
+            return out
+
+        return rotate([90,0,0])(
+            ports()
+        )
+    
+    def _cutaway(do):
+        if(do):
+            return left(od/2)(
+                cube([od, od*1.4, length*1.2 + endpadding*2], center=True)
+            )
+        else:
+            return empty()
+
+    # This block makes about as much sense as schnozzberries.
+    ct = 0
+    t = 0
+    tn = 1
+    if not top:
+        ct = oww*2
+        t = 1
+        tn = -1
+
+    c = 0
+    if not center:
+        c = length/2 + ct
+
+    
+    #(length*(1/7) and (1/14) reflects the length of the 3-chambered spool; at least this amount of
+    #space is needed for the valve to fully actuate, so long as the input
+    #and output adapters are flush with the inside of the valve. Endapdding
+    #is an optional variable, in the event that the adapters are NOT flush.
+    return up(c)(
+        (
+            pipe(length + endpadding*2, od, oww, center=True)
+            +
+            up(tn*length/2)(
+                mirror([0,0,t])(
+                    cap(od, oww)
+                )
+            )
+            -
+            endports()
+            -
+            midports()
+            -
+            _cutaway(cutaway)
+        )
+        +
+        up(length*(1/14))(
+            spool(length - length*(1/7), id, iww, 3, center=True)
+        )
+    )
+
 """---Assemblies---"""
 
 def assembly():
-    b = body()
-    h = holes()
-    return b - h
+
+    def _cutaway(do):
+        offset = 5
+
+        return forward(offset)(
+            cube([cylinderlength*5, offset*2, offset*2], center=True)
+        )
+            
+    # Main body and piston
+    def mainbody():
+        return (
+            rotate([0, 90, 0])(
+                body()
+            )
+            -
+            holes()
+            -
+            _cutaway(cutaway)
+        )
+
+    def five_valve_assembly(top=True):
+        return five_valve(6, spoolouterod, spoolouterww, spoolinnerod, spoolinnerww, top=top, center=False)
+
+    return (
+        mainbody()  
+        +
+        rotate([0,90,0])(
+            mainpiston()
+        )
+        +
+        forward(6)(
+            rotate([0,90,0])(
+                five_valve_assembly()
+                +
+                down(6 + spoolouterww*2)(
+                    five_valve_assembly(top=False)
+                )
+            )
+        )
+    )
+
 
 if __name__ == '__main__':
     out_dir = sys.argv[1] if len(sys.argv) > 1 else os.curdir
